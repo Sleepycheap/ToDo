ed13283aae98964506780c21a19288bea4773056
Sleepycheap <anthony.vaut@gmail.com>
2025-11-19 17:12:00 -0600

    fixing hiding tasks

--- src/Utils/tasks.js
+++ src/Utils/tasks.js
@@ -1,93 +1,80 @@
+const items = [];
 export const createTask = () => {
+  // Add Task button
   const button = document.querySelector('#task-add');
   button.addEventListener('click', (e) => {
     e.preventDefault();
     console.log('click');
     const container = document.querySelector('.content');
-    const parent = document.createElement('div');
-    parent.id = 'task-div';
-    container.appendChild(parent);
-    const taskAdd = document.querySelector('#task-add');
-    taskAdd.classList.add('hide');
+    // const parent = document.createElement('div');
+    // parent.id = 'task-div';
+    // parent.style.display = 'grid';
+    // container.appendChild(parent);
+    const taskAdd = container.querySelectorAll('#task-add');
+    let 
+    for (let i = 0; i === items.length; i++) {
+      items.push[i];
+      taskAdd[i].style.display = 'none';
+    }
     const card = document.querySelector('#task-card');
     card.style.display = 'none';
-    // const taskDiv = document.createElement('div');
-    // taskDiv.id = "task-div";
-    // content.appendChild(taskDiv);
-    const form = document.createElement('form');
-    form.id = 'create-task';
-    parent.appendChild(form);
-    const label = document.createElement('label');
-    label.setAttribute('for', 'task');
-    form.appendChild(label);
-    const input = document.createElement('input');
-    input.setAttribute('type', 'text');
-    input.id = 'task';
-    form.appendChild(input);
-    const button = document.createElement('button');
-    button.setAttribute('form', 'create-task');
-    button.setAttribute('type', 'submit');
-    button.classList.add('create');
-    button.textContent = 'Add';
-    parent.appendChild(button);
-    const reset = document.createElement('button');
-    reset.setAttribute('form', 'create-task');
-    reset.setAttribute('type', 'reset');
-    reset.classList.add('cancel');
-    reset.textContent = 'Cancel';
-    parent.appendChild(reset);
-    form.addEventListener('submit', (e) => {
+    const taskDiv = document.querySelector('#task-div');
+    taskDiv.style.display = 'grid';
+    const add = document.querySelector('.create');
+    add.addEventListener('click', (e) => {
       e.preventDefault();
       getTaskName();
     })
   })
 }
 
-// export const getTaskName = () => {
-//   const button = document.querySelector('form');
-//   button.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     const submitButton = e.target.elements[0];
-   
-//   })
-// }
+export const loadPage = () => {
 
+}
 
+const tasks = ['Test Task'];
 export const getTaskName = () => {
+  // e.preventDefault();
+  console.log(`testing taskname`);
   const content = document.querySelector('.content');
   const taskName = document.getElementById('task').value;
   const parent = document.querySelector('#task-div');
   const created = document.querySelector('#created-tasks');
   const add = document.querySelector('.create');
   const cancel = document.querySelector('.cancel');
-  const form = document.querySelector('form');
+  const form = document.querySelector('#task-div');
+  const taskList = document.querySelector('ul');
+  const li = document.createElement('li');
+  if (tasks.includes(taskName)) {
+    console.log('task already created');
+  } else {
+    li.id = 'task-card';
+    taskList.appendChild(li);
+    const button = document.createElement('button');
+    button.setAttribute('type', 'button');
+    button.classList.add('clear-task');
+    li.appendChild(button);
+    const p = document.createElement('p');
+    p.classList.add('task-p');
+    p.textContent = taskName;
+    li.appendChild(p);
+    tasks.push(taskName);
+    console.log(` Adding task: ${taskName}`);
+    console.log(`tasks after adding: ${tasks}`);
+  }
+  // tasks.push(taskName);
+  console.log(`tasks: ${tasks}`);
+  // content.removeChild(parent);
+  form.style.display = 'none';
+  // console.log(`testing: ${taskName}`);
   const card = document.querySelector('#task-card');
   card.style.display = 'flex';
-  // localStorage.setItem("task", taskName);
-  // const test = localStorage.getItem(task);
-  console.log(taskName);
-  // parent.removeChild(form);
-  // parent.removeChild(add);
-  // parent.removeChild(cancel);
-  content.removeChild(parent);
-  const taskList = document.querySelector('ul');
-  // taskList.id = 'task-list';
-  // created.appendChild(taskList);
-  const li = document.createElement('li');
-  li.id = 'task-card';
-  taskList.appendChild(li);
-  const button = document.createElement('button');
-  button.setAttribute('type', 'button');
-  button.classList.add('clear-task');
-  li.appendChild(button);
-  const p = document.createElement('p');
-  p.classList.add('task-p');
-  p.textContent = taskName;
-  li.appendChild(p);
+  const taskAdd = document.querySelector('#task-add');
+  taskAdd.style.display = 'grid';
 }
 
-export const clearTask = () => {
-  const button = document.querySelector('.clear-task');
+export const clearTask = (button) => {
+  // const button = document.querySelector('.clear-task');
   button.addEventListener('click', (e) => {
     e.preventDefault();
     const card = document.querySelector('#task-card');
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import "./style.css"
 import { createTask, getTaskName, clearTask } from "./Utils/tasks.js";
 
+const button = document.querySelector('.clear-task');
+const card = document.querySelector('#task-card');
+
 createTask();
 // getTaskName();
-clearTask();
+clearTask(button, card);
+
+// const add = document.querySelector('.create');
+// add.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   getTaskName();
+// });
