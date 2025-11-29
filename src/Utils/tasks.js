c58a5999cd580ba96746ce8fe648b64fe45ae7a9
Sleepycheap <anthony.vaut@gmail.com>
2025-11-29 14:41:30 -0600

    backup

--- src/Utils/tasks.js
+++ src/Utils/tasks.js
@@ -24,13 +24,9 @@ export const loadPage = () => {
 
 const tasks = ['Test Task'];
 export const getTaskName = () => {
-<<<<<<< HEAD
-  const tasks = [];
-=======
   const list = document.querySelector('#task-list');
   list.style.display = 'block';
   // console.log(`testing taskname`);
->>>>>>> c4ee2dd5cdfe72b3c90658ec8dffc57450e87b8f
   const content = document.querySelector('.content');
   const taskName = document.getElementById('task').value;
   const parent = document.querySelector('#task-div');
@@ -40,7 +36,7 @@ export const getTaskName = () => {
   const form = document.querySelector('#task-div');
   const taskList = document.querySelector('ul');
   const li = document.createElement('li');
-  if (tasks.includes(taskName)) {
+  if (Storage.includes(taskName)) {
     console.log('task already created');
   } else {
     li.id = 'task-card';
@@ -54,55 +50,19 @@ export const getTaskName = () => {
     p.textContent = taskName;
     li.appendChild(p);
     tasks.push(taskName);
+    localStorage.setItem("task", JSON.stringify(taskName));
   }
   form.style.display = 'none';
   const card = document.querySelector('#task-card');
-  const addTask = document.querySelector('#task-add');
   card.style.display = 'flex';
-<<<<<<< HEAD
-  window.localStorage.setItem("task", taskName);
-  // const test = localStorage.getItem(task);
-  // console.log(`test: ${typeof(taskName)}`);
-  // parent.removeChild(form);
-  // parent.removeChild(add);
-  // parent.removeChild(cancel);
-  content.removeChild(parent);
-  const taskList = document.querySelector('ul');
-  // taskList.id = 'task-list';
-  // created.appendChild(taskList);
-  const li = document.createElement('li');
-  li.id = 'task-card'
-  taskList.appendChild(li);
-  const button = document.createElement('button');
-  button.setAttribute('type', 'button');
-  button.classList.add('clear-task');
-  // button.addEventListener()
-  li.appendChild(button);
-  clearTask(button, card);
-  const p = document.createElement('p');
-  p.classList.add('task-p');
-  p.textContent = taskName;
-  li.appendChild(p);
-  addTask.classList.remove('hide');
-}
-
-export const clearTask = (button, card) => {
-
-=======
   const taskAdd = document.querySelector('#task-add');
   taskAdd.style.display = 'grid';
 }
 
 export const clearTask = (button) => {
->>>>>>> c4ee2dd5cdfe72b3c90658ec8dffc57450e87b8f
   button.addEventListener('click', (e) => {
     e.preventDefault();
-    for (const task of document.getElementsByClassName('clear-task')) {
-      const p = task.closest('li');
-      const closest = card.closest('li');
-      console.log(`closest: ${closest}`);
-      closest.style.display = 'none';
-      console.log(p);
-    }
+    const card = document.querySelector('#task-card');
+    card.style.display = 'none';
   })
 }
